20e168fbc0645db98144a1aada1f76aacab6e28d
unknown <sashamedoliz@gmail.com>
2021-04-10 00:53:05 +0300

    added task num 6

---
+++ index6.js
@@ -0,0 +1,24 @@
+"use strict"
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+  ];
+  
+  const calculateTotalPrice = function(allProdcuts, productName) {
+      let result=0;
+    for (let i=0; i<allProdcuts.length; i++){
+        if(allProdcuts[i].name===productName){
+            result=allProdcuts[i].price*allProdcuts[i].quantity;
+            
+        }
+    }
+    return result;
+  };
+  
+  
+
+  console.log(calculateTotalPrice(products, 'Радар')); // 5200
+  
+  console.log(calculateTotalPrice(products, 'Дроид')); // 2800
